0ab8330b18eaa42bd44c87987dc1d4288782a976
JV-Repo <joao.assumpo@gmail.com>
2022-05-26 14:28:35 -0300

    PhP OOP para conexões com mysql

--- telaCadastro.php
+++ telaCadastro.php
@@ -1,45 +1,51 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">   
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
+
 <body>
-    <?php require 'conexao.php'?>
-    <?php 
-        $nome = $_POST['nome'];
-        $login = $_POST['login'];
-        $senha = md5($_POST['senha']);
-        $dataNasc = $_POST['dataNasc'];
-        $email = $_POST['email'];
-        $cpf = $_POST['cpf'];
-        $telefone = $_POST['telefone'];
+    <?php require 'conexao.php' ?>
+    <?php
+    $nome = $_REQUEST['nome'];
+    $login = $_REQUEST['login'];
+    $senha = md5($_REQUEST['senha']);
+    $dataNasc = $_REQUEST['dataNasc'];
+    $email = $_REQUEST['email'];
+    $cpf = $_REQUEST['cpf'];
+    $telefone = $_REQUEST['telefone'];
 
-        // Cria conexão
-		$conn = mysqli_connect($servername, $username, $password, $database);
+    // Cria conexão
+    $conn = new mysqli($servername, $username, $password, $database);
 
-		// Verifica conexão
-		if (!$conn) {
-			die("Connection failed: " . mysqli_connect_error());
-		}
-		// Configura para trabalhar com caracteres acentuados do português
-		mysqli_query($conn,"SET NAMES 'utf8'");
-		mysqli_query($conn,'SET character_set_connection=utf8');
-		mysqli_query($conn,'SET character_set_client=utf8');
-		mysqli_query($conn,'SET character_set_results=utf8');
+    // Verifica conexão
+    if ($conn->connect_error) {
+        die("Conexão Falhou: " . $conn->connect_error);
+    }
+    // Configura para trabalhar com caracteres acentuados do português
+    $conn->query("SET NAMES 'utf8'");
+    $conn->query('SET character_set_connection=utf8');
+    $conn->query('SET character_set_client=utf8');
+    $conn->query('SET character_set_results=utf8');
 
-		// Faz Select na Base de Dados
-		$sql = "BEGIN;
-            INSERT INTO Usuario (Nome, Login, Senha, DataNasc, Email, CPF, Telefone) VALUES ('$nome','$login','$senha','$dataNasc','$email', '$cpf');
+    // Faz Insert na Base de Dados
+    $sql = "BEGIN;
+            INSERT INTO Usuario (Nome, Login, Senha, DataNasc, Email, CPF, Telefone) 
+                VALUES ('$nome','$login','$senha','$dataNasc','$email', '$cpf');
             INSERT INTO Telefone (CodUsu, NumTel) VALUES (LAST_INSERT_ID(), '$telefone');
-         COMMIT;";
+            COMMIT;";
 
-         $mysql_query($query,$conexao);
-         echo "Seu cadastro foi realizado com sucesso!<br>Agradecemos a atenção.";
-    ?> 
-		
+    $conn->query($sql);
+
+    echo "Seu cadastro foi realizado com sucesso!<br>Agradecemos a atenção.";
+    $conn->close();
+    ?>
+
 
 
 </body>
+
 </html>
